ac87378161c440593208c4eeb7dd5b36043385da
Charalampos-Thomaidis <risxapsyy@gmail.com>
2023-06-12 02:13:43 +0300

    discord bot. not finished

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,101 @@
+﻿using System.Threading.Tasks;
+using System.Linq;
+using Discord;
+using Discord.Commands;
+using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Videos.Streams;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("song", RunMode = RunMode.Async)]
+        public async Task HandlePlayCommand([Remainder] string arguments)
+        {
+            var voiceChannel = (Context.User as IGuildUser)?.VoiceChannel;
+
+            if (voiceChannel == null)
+            {
+                await Context.Channel.SendMessageAsync("You must be in a voice channel to use this command.");
+                return;
+            }
+
+            var youtube = new YoutubeClient();
+
+            var searchResults = await youtube.Search.GetVideosAsync(arguments);
+
+            var video = searchResults.FirstOrDefault();
+
+            if (video == null)
+            {
+                await Context.Channel.SendMessageAsync("No search results found.");
+                return;
+            }
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+
+            var audioStreamInfo = streamManifest.GetAudioOnlyStreams().FirstOrDefault();
+
+            if (audioStreamInfo == null)
+            {
+                await Context.Channel.SendMessageAsync("Failed to retrieve audio stream for the video.");
+                return;
+            }
+
+            var streamInfoSet = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+            var audioStreamInfoWithHighestBitrate = streamInfoSet.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            if (audioStreamInfoWithHighestBitrate == null)
+            {
+                await Context.Channel.SendMessageAsync("Failed to retrieve audio stream for the video.");
+                return;
+            }
+
+            var filePath = $"{video.Id}.{audioStreamInfoWithHighestBitrate.Container.Name}";
+
+            await youtube.Videos.Streams.DownloadAsync(audioStreamInfoWithHighestBitrate, filePath);
+
+            var response = $"Now playing: {video.Title}\n{video.Url}";
+            await Context.Channel.SendMessageAsync(response);
+        }
+
+        [Command("summon", RunMode = RunMode.Async)]
+        public async Task HandleSummonBotCommand(IVoiceChannel channel = null)
+        {
+            channel ??= (Context.User as IGuildUser)?.VoiceChannel;
+
+            if (channel == null)
+            {
+                await ReplyAsync("You must be in a voice channel to use this command.");
+                return;
+            }
+
+            await ReplyAsync("Joined the voice channel.");
+            await channel.ConnectAsync();
+        }
+
+        [Command("leave", RunMode = RunMode.Async)]
+        public async Task HandleLeaveBotCommand(IVoiceChannel channel = null)
+        {
+            channel ??= (Context.User as IGuildUser)?.VoiceChannel;
+
+            if (channel == null)
+            {
+                await ReplyAsync("The bot is not currently in a voice channel.");
+                return;
+            }
+
+            await ReplyAsync("Left the voice channel.");
+
+            await channel.DisconnectAsync();
+        }
+
+        [Command("commands")]
+        public async Task HandleExplanationCommands()
+        {
+            await ReplyAsync("!song (name song)\n !summon (ill join your vc)\n !leave (ill leave your vc)");
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main() => new Program().RunAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunAsync()
+        {
+            var config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.DirectMessages
+                                 | GatewayIntents.GuildMessages
+                                 | GatewayIntents.Guilds
+                                 | GatewayIntents.GuildMembers
+                                 | GatewayIntents.MessageContent
+                                 | GatewayIntents.GuildVoiceStates
+            };
+
+            _client = new DiscordSocketClient(config);
+
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "token";
+
+            _client.Log += Client_Log;
+
+            _client.Ready += Client_Ready;
+
+            await InstallCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready()
+        {
+            Console.WriteLine($"Logged in as {_client.CurrentUser}");
+            return Task.CompletedTask;
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
